6e02fbdd3fa9b784eef6a09397ef57c1950d982d
pullso <p.dubitskiy@ya.ru>
2020-10-07 23:56:21 +0300

    finish webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as $ from 'jquery'
 import { Post } from '@models/Post'
 import './styles/styles.css'
+import './styles/styl.styl'
+import './styles/sass.sass'
+import './babel'
 import json from '@/assets/json'
 import xml from '@/assets/data.xml'
 import csv from '@/assets/data.csv'
@@ -13,3 +16,7 @@ $('pre').addClass('code').html(post.toString())
 console.log(json, ':json')
 console.log(xml, ':xml')
 console.log(csv, ':csv')
+
+import('lodash').then(_=>{
+  console.log(_.random(0,42,false), ':_.random(0,42,false)')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,113 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const FileLoader = require('file-loader')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require(
+  'optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  }
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+  return config
+}
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true,
+      },
+    }, 'css-loader',
+  ]
+  if (extra) loaders.push(extra)
+  
+  return loaders
+}
+
+const babelOptions = (preset) => {
+  const opt = {
+    presets: ['@babel/preset-env'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+  }
+  if (preset) opt.presets.push(preset)
+  return opt
+}
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: babelOptions(),
+    }]
+  
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+  return loaders
+}
+
+const plugins = () => {
+  const base = [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        }],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css'),
+    }),
+  ]
+  
+  if (isProd) {
+    base.push(new BundleAnalyzerPlugin())
+  }
+  
+  return base
+}
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './index.js',
+    main: ['@babel/polyfill', './index.js'],
     analytics: './analytics.js',
   },
   output: {
-    filename: '[name].[contenthash].js',
+    filename: filename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  plugins: plugins(),
+  optimization: optimization(),
   devServer: {
-    port: 4200
+    port: 4200,
+    hot: isDev,
   },
   resolve: {
     extensions: ['.js', '.json', '.png'],
@@ -38,7 +120,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: cssLoaders(),
+      },
+      {
+        test: /\.styl$/,
+        use: cssLoaders('stylus-loader'),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
       },
       {
         test: /\.(png|jpeg|jpg|gif|svg)$/,
@@ -56,6 +146,12 @@ module.exports = {
         test: /\.csv$/,
         use: ['csv-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
     ],
   },
+  devtool: isDev ? 'source-map' : '',
 }
