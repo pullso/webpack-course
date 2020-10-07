bd578b00eaa4b9629610ee6161ad9872e118557c
pullso <p.dubitskiy@ya.ru>
2020-10-07 11:11:32 +0300

    add examples

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import { Post } from './Post'
+import * as $ from 'jquery'
+import { Post } from '@models/Post'
+import './styles/styles.css'
+import json from '@/assets/json'
+import xml from '@/assets/data.xml'
+import csv from '@/assets/data.csv'
+import webpackLogo from './assets/webpack-logo'
 
-const post = new Post('Webpack post title')
+const post = new Post('Webpack post title', webpackLogo)
 
-console.log('post to string:',post.toString())
+$('pre').addClass('code').html(post.toString())
+
+console.log(json, ':json')
+console.log(xml, ':xml')
+console.log(csv, ':csv')
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,61 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FileLoader = require('file-loader')
 
 module.exports = {
-  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
   entry: {
-    main: './src/index.js',
-    analytics: './src/analytics.js'
+    main: './index.js',
+    analytics: './analytics.js',
   },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname,'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devServer: {
+    port: 4200
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(ttf|woff2|woff|eof)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader'],
+      },
+      {
+        test: /\.csv$/,
+        use: ['csv-loader'],
+      },
+    ],
+  },
 }
